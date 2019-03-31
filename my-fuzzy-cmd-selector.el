412e62dd970fe20486dda1403190e4d9a537d5b7
vitor quintanilha barbosa <vitorqb@gmail.com>
2019-03-31 11:46:07 +0200

    NEW Adds function `mfcs-add-command`
    - NEW Creates function `mfcs-add-command` to add a command.
    - EXTRA extracts `mfcs--get-desc` and `mfcs--get-func` as top-level
      functions

--- my-fuzzy-cmd-selector.el
+++ my-fuzzy-cmd-selector.el
@@ -15,6 +15,7 @@
 (require 'ivy)
 (require 'dash)
 (require 'dash-functional)
+(require 'cl-lib)
 
 ;;; code
 (defvar mfcs-commands '()
@@ -24,17 +25,27 @@
    '((:description \"Run compile\"
       :command (lambda () (interactively) (call-interactively compile))))")
 
+(defun mfcs--get-desc (x) (plist-get x :description))
+(defun mfcs--get-func (x) (plist-get x :command))
+
+(cl-defun mfcs-add-command (&key description command)
+  "Adds a command to mfcs-commands if no other command with the
+  same description is there."
+  (when (not (-some? (-compose (-partial #'string-equal description)
+                               #'mfcs--get-desc)
+                     mfcs-commands))
+    (cl-pushnew `(:description ,description :command ,command) mfcs-commands)))
+
 (defun mfcs-call ()
   "Prompts the user to select from mfcs-commands and runs the selected function"
   (interactive)
   (unless mfcs-commands (error "mfcs-commands is empty"))
-  (-let* ((get-desc (-rpartial 'plist-get :description))
-          (get-func (-rpartial 'plist-get :command))
-          (chosen-desc
-           (ivy-read "Command: " (-map get-desc mfcs-commands) :require-match t)))
+  (-let* ((chosen-desc (ivy-read "Command: "
+                                 (-map #'mfcs--get-desc mfcs-commands)
+                                 :require-match t)))
     (->> mfcs-commands
-         (-first (lambda (x) (string= (funcall get-desc x) chosen-desc)))
-         (funcall get-func)
+         (-first (lambda (x) (string= (funcall #'mfcs--get-desc x) chosen-desc)))
+         mfcs--get-func
          funcall-interactively)))
 
 (provide 'my-fuzzy-cmd-selector)
